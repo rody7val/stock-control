4868308bfac4ee2b035923ceb85ee48f6b5adbd2
rody7val <rod7val@gmail.com>
2016-11-26 18:33:22 -0300

    Informe de stock inportando un excel

--- dist/routes/api.js
+++ dist/routes/api.js
@@ -12,14 +12,15 @@ module.exports = function (app, express) {
     api.param('itemId', itemController.load);
 
     // item
-    api.get('/find', itemController.find);
-    api.get('/items', itemController.all);
+    api.get('/item/find', itemController.find);
     api.get('/item/new', itemController.new);
     api.post('/item/new', itemController.create);
     api.get('/item/:itemId', itemController.show);
     api.get('/item/:itemId/edit', itemController.edit);
     api.put('/item/:itemId/edit', itemController.update);
     api.delete('/item/:itemId', itemController.delete);
+    api.get('/items', itemController.all);
+    api.get('/items/stock', itemController.stock);
 
     // Retornar rutas API.
     return api;
